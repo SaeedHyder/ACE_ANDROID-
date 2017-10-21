57e3c68129fb457260382d143760ad3c24d43eab
shahsyed <shahsyed@axact.com>
2017-10-21 07:07:48 -0700

    Signed-off-by: shahsyed <shahsyed@axact.com>

--- app/src/main/java/com/app/ace/fragments/TrainerSignUpForm2Fragment.java
+++ app/src/main/java/com/app/ace/fragments/TrainerSignUpForm2Fragment.java
@@ -53,7 +53,8 @@ import roboguice.inject.InjectView;
  * Created by khan_muhammad on 3/13/2017.
  */
 
-public class TrainerSignUpForm2Fragment extends BaseFragment implements View.OnClickListener, CompoundButton.OnCheckedChangeListener, MainActivity.ImageSetter, IGetLocation {
+public class TrainerSignUpForm2Fragment extends BaseFragment implements View.OnClickListener,
+        CompoundButton.OnCheckedChangeListener, MainActivity.ImageSetter, IGetLocation {
 
 
     @InjectView(R.id.btnSignUp)
